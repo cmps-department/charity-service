aca2ef5fa6c96dee6ea7c70aee47f07acd9b2281
Viktor <lazvik@ukr.net>
2023-04-14 15:08:39 +0300

    implemented requested changes #2

--- src/main/java/cmps/charityservice/SampleController.java
+++ src/main/java/cmps/charityservice/SampleController.java
@@ -1,19 +1,20 @@
 package cmps.charityservice;
 
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/")
-
 public class SampleController {
+
     @GetMapping
-    public Map<String, Object> currentUser(OAuth2AuthenticatedPrincipal oAuth2AuthenticatedPrincipal){
-        // after authorization through Google should show a list of properties, received from Map
-        return oAuth2AuthenticatedPrincipal.getAttributes();
+    public Map<String, Object> currentUser(JwtAuthenticationToken principal) {
+        return principal.getTokenAttributes();
     }
 }
