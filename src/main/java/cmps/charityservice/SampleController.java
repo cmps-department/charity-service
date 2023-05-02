f49034f2da22b71d3333dce1f335644701b2216c
Санжара Владислав Олегович <vladyslav.sanzhara@infiz.khpi.edu.ua>
2023-05-02 12:54:07 +0300

    added changelog/master.xml directory to resourcesec added the ability to grant users administrator status

--- src/main/java/cmps/charityservice/SampleController.java
+++ src/main/java/cmps/charityservice/SampleController.java
@@ -1,20 +1,36 @@
 package cmps.charityservice;
 
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.Map;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/")
 public class SampleController {
+    @Autowired
+    private UsersResource usersResource;
 
     @GetMapping
     public Map<String, Object> currentUser(JwtAuthenticationToken principal) {
         return principal.getTokenAttributes();
+    }
+
+    @Secured("ROLE_ADMIN")
+    @PostMapping("/makeUserAdmin")
+    public void makeUserAdmin(@RequestParam String userId) {
+        UserRepresentation user = usersResource.get(userId).toRepresentation();
+
+        RealmResource realmResource = keycloak.realm(usersRealm);
+        RoleRepresentation adminRole = realmResource.roles().get("admin").toRepresentation();
+        usersResource.get(userId).roles().realmLevel().add(Collections.singletonList(adminRole));
     }
 }
