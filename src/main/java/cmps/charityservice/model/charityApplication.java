1f133ca059ce845ce337f17daf1999577f7268e4
Viktor <lazvik@ukr.net>
2023-05-04 18:04:04 +0300

    added initial schema and model
    added initial schema and model

---
+++ src/main/java/cmps/charityservice/model/charityApplication.java
@@ -0,0 +1,25 @@
+package cmps.charityservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class charityApplication {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private String id;
+    private String author;
+    private String purpose;
+    private String target_amount;
+    private String short_description;
+    private String full_description;
+}
