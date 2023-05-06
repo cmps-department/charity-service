85b4388c6f7ba43979f15bb35000a00aa060b6cc
Viktor <lazvik@ukr.net>
2023-05-06 13:55:58 +0300

    added Category enum
    added Category enum with list of categories

---
+++ src/main/java/cmps/charityservice/model/Category.java
@@ -0,0 +1,15 @@
+package cmps.charityservice.model;
+
+public enum Category {
+    ARMY,
+    REFUGEES,
+    ANIMALS,
+    ELDERY,
+    DISABLITY,
+    ONKOLOGY,
+    ORPHANS,
+    HOMELESS,
+    EKOORG,     //EKO ORGANIZATIONS
+    COMMORG,    //COMMUNITY ORGANIZATIONS
+    INTORG,     //INTERNATIONAL ORGANIZATIONS
+}
